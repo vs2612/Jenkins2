8421ce860e78a9e482d9674eae0ec849fac4d166
vs2612 <vs6686949@gmail.com>
2025-03-11 13:54:04 +0530

    fixes added

--- src/test/java/runners/LoginRunner.java
+++ src/test/java/runners/LoginRunner.java
@@ -4,15 +4,12 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "stepDefinitions",
+        glue = "stepDefinitions", // This should match the package where LoginSteps is defined
         tags = "@Login",
-        plugin = {"pretty", "json:target/cucumber-reports/cucumber.json"}
-
+        plugin = {"pretty", "json:target/cucumber-reports/cucumber2.json"}
 )
 public class LoginRunner extends AbstractTestNGCucumberTests {
-
-
 }
+
